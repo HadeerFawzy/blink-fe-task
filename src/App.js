f66436134e80236dc8952daf85dc732c0d2602ce
Hadeer Fawzy <hadeerfawzymahmoud92@gmail.com>
2021-01-15 13:17:13 +0200

    Override Material UI theme with custom breakpoint - colors - font-family

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { ThemeProvider } from "@material-ui/core";
+import theme from "./config/theme";
 import Layout from './components/Layout';
 
 const App = () => {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <Layout/>
-    </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/config/theme.js
@@ -0,0 +1,48 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const defaultTheme = createMuiTheme();
+
+let theme = createMuiTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 320,
+      md: 600,
+      lg: 1024,
+      xl: 1200,
+      xxl: 1920,
+    },
+  },
+  palette: {
+    primary: {
+      main: "#4D7CFE",
+      dark: "#3159C8",
+    },
+    secondary: {
+      main: "#778CA2"
+    },
+    success: {
+      main: "#6DD230",
+      dark: "#5CAD2C",
+    },
+    error: {
+      main: "#FE4D5C",
+    }
+  },
+  typography: {
+    fontFamily: "'Rubik', Roboto",
+    fontFamilySecondary: "'Roboto Slab', serif",
+  },
+  props: {
+    MuiLink: {
+      underline: "none",
+      color: "textPrimary",
+    },
+    MuiContainer: {
+      maxWidth: "xl",
+    }
+  },
+  spacing: (factor) => `${0.5 * factor}rem`,
+});
+
+export default theme;
