54fbd7694405a7e62f9c37207adf04422a638afb
Hadeer Fawzy <hadeerfawzymahmoud92@gmail.com>
2021-01-15 12:07:20 +0200

    Create Sidebar Component

---
+++ src/components/Layout/Sidebar/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sidebar = () => {
+  return (
+    <div>
+      Sidebar goes here
+    </div>
+  );
+}
+
+export default Sidebar;
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Sidebar from './Sidebar';
 
 const Layout = () => {
   return (
-    <div className="layout">
-      Layout goes here
+    <div>
+      <Sidebar/>
     </div>
   );
 }
